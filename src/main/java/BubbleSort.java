2b231d14b278a9a4d40d483e1d0fdb52f3395e9e
cuizhiheng <52915839+zhhengcui@users.noreply.github.com>
2020-10-27 19:25:34 +0800

    更改数据2

--- src/main/java/BubbleSort.java
+++ src/main/java/BubbleSort.java
@@ -27,7 +27,7 @@ public class BubbleSort {
     }
 
     public static void main(String[] args) {
-        int[] data = { 9, -16, 21, 23, -30, -49, 21, 30, 50};
+        int[] data = { 9, -16, 21, 23, -30, -49, 21, 30, 20};
         System.out.println("排序之前：\n" + Arrays.toString(data));
 
         bubbleSort(data);
